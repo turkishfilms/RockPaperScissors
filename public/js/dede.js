f890fd8268aab870159d0e18f70fde8b6ff3d54e
Reginald Robinson <ddrbnsn@gmail.com>
2023-05-24 17:31:05 -0700

    okay

--- public/js/dede.js
+++ public/js/dede.js
@@ -1,68 +1,80 @@
-/**
- * TODONE: introducce game
- * TODONE:stats
- * TODO:ask for input
- * TODO:get computers answrr
- * 
- * TODO:compare
- * 
- * TODO:assign points
- * TODO:Check winner
- * TODO:if no abck to stats
- * TODO:if yes show winner, wait
- * TODO:
- * TODO:
- * TODO:
- * TODO:
 
- */
+const introGame = () => alert(`"Welcome to Salem's game of Rock, Paper, Scissors\nWin 2 out of the 3 rounds to be declared a winner"'`)
 
-const introGame = () => alert('howdy')
+const showGameStats = (scores) => alert("Score:  " + scores.playerScore + " | " + scores.computerScore + "\n");
 
-const showGameStats = (scores) => alert(`current stats are${scores}`)
-
-const getPlayerGesture = (gestures) => {
-    const userGesture = alert(`pick dude`)
-    if (gestures.findIndex(userGesture) == -1) return "dq"
+const getPlayerGesture = (gestures, round) => {
+    const userGesture = prompt("Round " + round + "\nRock, paper, or scissors? ").toLowerCase()
+    if (gestures.findIndex((gs) => gs == userGesture) == -1) return "dq"
     return userGesture
 }
 
-const getComputerGesture = (gestures) => { return gestures[Math.floor(Math.random(0, gestures.length))] }
+const getComputerGesture2 = (gestures) => { return gestures[Math.floor(Math.random() * gestures.length)] }
 
-const compareGestures = (playedGestures, gestrures) => {
-    const p1 = playedGestures[0]
-    if (gestures[1] == gestrures) console.log("yay")
+
+const getComputerGesture = () => "scissors"
+
+const compareGestures = (playedGestures, gestures) => { //returns 0 0.5 1
+    const p1 = playedGestures[0],
+        p2 = playedGestures[1]
+    alert("You chose " + p1 + "\nComputer chose " + p2);
+
+    if (p1 == 'dq') {
+        alert("Disqualified!")
+        return 0
+    }
+    const beatsP1 = (gestures.findIndex((gs) => gs == p1) + 1)
+    console.log("bp1", beatsP1, "p2", p2)
+    const t = (p2 == gestures[(beatsP1 % 3)] ? 0 : 1)
+    console.log(t)
+    if (p2 == p1) return 0.5
+    else return t
 }
 
-//does two things :( also ""player"" eww
-const increaseScore = winner => winner == 'player' ? playerScore++ : computerScore++
+const increaseScore = (playerWon, scores) => {
+    if (playerWon == 0.5) return
+    playerWon ? scores.playerScore++ : scores.computerScore++
+}
 
-const checkGameWinner = (scores, maxScore) => {
-    const playerScore = scores[0],
-        computerScore = scores[1]
-    if (playerScore == maxScore) return "player"
-    if (computerScore == maxScore) return "computer"
+const checkGameWinner = (score, maxScore) => {
+    if (score.playerScore == maxScore) return "player"
+    if (score.computerScore == maxScore) return "not player"
     return 0
 }
 
-const displayWinner = winner => alert(`${winner} is winner, the other guy is trash`)
+const displayGameWinner = playerWon => {
+    if (playerWon == "player") alert("Congrats, you won!!!");
+    else alert("Ruh roh, you lost...")
+}
 
-const rockPaperScissorsGame = (maxScore, []) => {
-    const GESTURES = ['rock', 'paper', 'scissors']
-    const MAXSCORE = 2
+const displayHandWinner = playerWon => {
+    if (playerWon == 0.5) alert("It's a draw!")
+    else alert(`Round ${playerWon ? "Won" : "Lost"}!`)
+}
 
-    let playerScore = 0
-    let computerScore = 0
-    let winnner
+const rockPaperScissorsGame = (maxScore, gs) => {
+    const GESTURES = gs || ['rock', 'paper', 'scissors']
+    const MAXSCORE = maxScore || 2
 
-    while (checkGameWinner([playerScore, computerScore], MAXSCORE)) {
-        showGameStats([playerScore, computerScore])
+    let round = 0
+    const scores = {
+        playerScore: 0,
+        computerScore: 0
+    }
+
+    while (!checkGameWinner(scores, MAXSCORE)) {
+        showGameStats(scores, round)
         const compG = getComputerGesture(GESTURES)
         const playerG = getPlayerGesture(GESTURES)
-        const winner = compareGestures([playerG, compG])
-        increaseScore(winner)
-        winnner = checkGameWinner([playerScore, computerScore], MAXSCORE)
+        const winner = compareGestures([playerG, compG], GESTURES)
+        increaseScore(winner, scores)
+        displayHandWinner(winner)
+        round++
     }
-    displayWinner(winnner)
-    alert("play again")
+
+    displayGameWinner(checkGameWinner(scores, MAXSCORE))
+    if (prompt("play again?") == "yes") rockPaperScissorsGame()
+    else alert("fine i didnt like you anyway")
 }
+
+rockPaperScissorsGame()
