10f8f75ce4224a85dad162a8b8c9291dfdf60169
Reginald Robinson <ddrbnsn@gmail.com>
2023-05-24 17:50:12 -0700

    rearranged again

--- public/js/dede.js
+++ public/js/dede.js
@@ -1,11 +1,38 @@
 
-const introGame = () => alert(`"Welcome to Salem's game of Rock, Paper, Scissors\nWin 2 out of the 3 rounds to be declared a winner"'`)
-
-
 
 
 const rockPaperScissorsGame = (maxScore, gs) => {
 
+    const GESTURES = gs || ['rock', 'paper', 'scissors']
+    const MAXSCORE = maxScore || 2
+
+    let round = 0
+    const scores = {
+        playerScore: 0,
+        computerScore: 0
+    }
+
+
+    const play = () => {
+        introGame()
+
+        while (!checkGameWinner(scores, MAXSCORE)) {
+            showGameStats(scores, round)
+            const compG = getComputerGesture(GESTURES)
+            const playerG = getPlayerGesture(GESTURES)
+            const winner = compareGestures([playerG, compG], GESTURES)
+            increaseScore(winner, scores)
+            displayHandWinner(winner)
+            round++
+        }
+        displayGameWinner(checkGameWinner(scores, MAXSCORE))
+        if (prompt("play again?") == "yes") rockPaperScissorsGame()
+        else alert("fine i didnt like you anyway")
+    }
+
+    //supporting functions
+
+    const introGame = () => alert(`"Welcome to Salem's game of Rock, Paper, Scissors\nWin 2 out of the 3 rounds to be declared a winner"'`)
 
     const showGameStats = (scores) => alert("Score:  " + scores.playerScore + " | " + scores.computerScore + "\n");
 
@@ -20,21 +47,19 @@ const rockPaperScissorsGame = (maxScore, gs) => {
 
     const getComputerGesture2 = () => "scissors"
 
-    const compareGestures = (playedGestures, gestures) => { //returns 0 0.5 1
+    const compareGestures = (playedGestures, gestures) => {
         const p1 = playedGestures[0],
             p2 = playedGestures[1]
+
         alert("You chose " + p1 + "\nComputer chose " + p2);
 
         if (p1 == 'dq') {
             alert("Disqualified!")
             return 0
         }
-        const beatsP1 = (gestures.findIndex((gs) => gs == p1) + 1)
-        console.log("bp1", beatsP1, "p2", p2)
-        const t = (p2 == gestures[(beatsP1 % 3)] ? 0 : 1)
-        console.log(t)
+
         if (p2 == p1) return 0.5
-        else return t
+        else return (p2 == gestures[((gestures.findIndex((gs) => gs == p1) + 1) % 3)] ? 0 : 1)
     }
 
     const increaseScore = (playerWon, scores) => {
@@ -58,29 +83,7 @@ const rockPaperScissorsGame = (maxScore, gs) => {
         return 0
     }
 
-    //game code
-    const GESTURES = gs || ['rock', 'paper', 'scissors']
-    const MAXSCORE = maxScore || 2
-
-    let round = 0
-    const scores = {
-        playerScore: 0,
-        computerScore: 0
-    }
-
-    while (!checkGameWinner(scores, MAXSCORE)) {
-        showGameStats(scores, round)
-        const compG = getComputerGesture(GESTURES)
-        const playerG = getPlayerGesture(GESTURES)
-        const winner = compareGestures([playerG, compG], GESTURES)
-        increaseScore(winner, scores)
-        displayHandWinner(winner)
-        round++
-    }
-    displayGameWinner(checkGameWinner(scores, MAXSCORE))
-    if (prompt("play again?") == "yes") rockPaperScissorsGame()
-    else alert("fine i didnt like you anyway")
-
+    play()
 }
 
 rockPaperScissorsGame()
